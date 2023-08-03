c8ddbec8f1f1933792dc38f6c493ce7e56f29177
Efe Mesudiyeli <efemesudiyeli0@gmail.com>
2023-08-03 16:16:03 +0300

    generateMetadata added for browser title

--- app/blog/[slug]/page.js
+++ app/blog/[slug]/page.js
@@ -1,5 +1,12 @@
 import BlogContentContainer from "@/containers/BlogContent";
-import { getAllPostSlugs } from "@/pages/api";
+import { getAllPostSlugs, getPostData } from "@/pages/api";
+
+export async function generateMetadata({ params }) {
+  const postData = getPostData(params.slug);
+  return {
+    title: `${postData.postData.data.title}`,
+  };
+}
 
 export default function BlogTemplate({ params }) {
   return <BlogContentContainer slug={params.slug} />;
